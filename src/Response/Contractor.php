bb997496d5e7fabcb0defd97fdcde62b732fbe45
Maksim Khramov <maxim.khramov@mail.ru>
2021-11-01 15:20:15 +0300

    Fix email for contractor dto

--- src/Response/Contractor.php
+++ src/Response/Contractor.php
@@ -6,16 +6,16 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class Contractor extends DataTransferObject
 {
-    public int    $id;
-    public string $first_name;
-    public string $last_name;
-    public string $phone;
-    public string $email;
-    public string $inn;
-    public string $kind;
-    public bool   $suspended;
-    public array  $contracts;
-    public array  $id_cards;
-    public string $moi_nalog_status;
-    public array  $bank_details;
+    public int     $id;
+    public string  $first_name;
+    public string  $last_name;
+    public string  $phone;
+    public ?string $email = null;
+    public string  $inn;
+    public string  $kind;
+    public bool    $suspended;
+    public array   $contracts;
+    public array   $id_cards;
+    public string  $moi_nalog_status;
+    public array   $bank_details;
 }
