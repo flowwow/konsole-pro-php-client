23c9431ea390325515c411f00804ac884c47e938
Maksim Khramov <maxim.khramov@mail.ru>
2021-11-08 16:25:05 +0300

    Make nullable date field for doc dto

--- src/Response/Doc.php
+++ src/Response/Doc.php
@@ -6,16 +6,16 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class Doc extends DataTransferObject
 {
-    public int    $id;
-    public string $status;
-    public string $title;
-    public string $date;
-    public int    $contractor_id;
-    public ?int   $contract_id = null;
-    public string $kind;
-    public ?int   $template_id = null;
-    public        $number;
-    public string $url;
-    public array  $data;
-    public int    $created;
+    public int     $id;
+    public string  $status;
+    public string  $title;
+    public ?string $date        = null;
+    public int     $contractor_id;
+    public ?int    $contract_id = null;
+    public string  $kind;
+    public ?int    $template_id = null;
+    public         $number;
+    public string  $url;
+    public array   $data;
+    public int     $created;
 }
