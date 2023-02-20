7bcf812523f165c7fc0392cb043ef71b31299581
Maksim Khramov <maxim.khramov@mail.ru>
2023-02-20 12:37:22 +0300

    Add account statement support

---
+++ src/Response/ResponseV2NominalAccountStatements.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Flowwow\KonsolPro\Response;
+
+/**
+ * DTO-объект для ответа v2/nominal_account/statement
+ */
+class ResponseV2NominalAccountStatements extends BaseResponse
+{
+    public int     $id;
+    public string  $date_from;
+    public string  $date_to;
+    public string  $format;
+    public string  $status;
+    public ?string $url;
+}
